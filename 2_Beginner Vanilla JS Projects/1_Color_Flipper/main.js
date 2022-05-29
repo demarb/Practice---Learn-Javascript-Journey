10b79dde5398998bcab3bc4c51f188d4572ee9b5
demarb <demarcbrown01@gmail.com>
2022-05-29 19:55:37 -0400

    Start color flipper project

---
+++ 2_Beginner Vanilla JS Projects/1_Color_Flipper/main.js
@@ -0,0 +1,58 @@
+const colorObjs = [
+    {
+        id: 1,
+        colorName: "Red",
+        hexValue: "#FF0000"
+    },
+    {
+        id: 2,
+        colorName: "Blue",
+        hexValue: "#0000FF"
+    },
+    {
+        id: 3,
+        colorName: "Purple",
+        hexValue: "#800080"
+    },
+    {
+        id: 4,
+        colorName: "Yellow",
+        hexValue: "#FFFF00"
+    },
+    {
+        id: 5,
+        colorName: "Lime",
+        hexValue: "#00FF00"
+    },
+    {
+        id: 6,
+        colorName: "Pink",
+        hexValue: "#FFC0CB"
+    },
+    {
+        id: 7,
+        colorName: "Orange",
+        hexValue: "#FFA500"
+    },
+    {
+        id: 8,
+        colorName: "Olive",
+        hexValue: "#808000"
+    },
+    {
+        id: 9,
+        colorName: "Grey",
+        hexValue: "#808080"
+    },
+    {
+        id: 10,
+        colorName: "Indigo",
+        hexValue: "#4B0082"
+    },
+    
+]
+
+// const changeBackground => {
+
+//     ;
+// }
