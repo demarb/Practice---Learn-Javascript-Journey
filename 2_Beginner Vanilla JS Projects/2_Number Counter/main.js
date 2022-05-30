f053faadd6c1bc6e01ba265af710ce678745160b
demarb <demarcbrown01@gmail.com>
2022-05-30 14:07:20 -0400

    Finish number counter

--- 2_Beginner Vanilla JS Projects/2_Number Counter/main.js
+++ 2_Beginner Vanilla JS Projects/2_Number Counter/main.js
@@ -1,85 +1,48 @@
-const colorObjs = [
-    {
-        id: 0,
-        colorName: "Red",
-        hexValue: "#FF0000"
-    },
-    {
-        id: 1,
-        colorName: "Blue",
-        hexValue: "#0000FF"
-    },
-    {
-        id: 2,
-        colorName: "Purple",
-        hexValue: "#800080"
-    },
-    {
-        id: 3,
-        colorName: "Yellow",
-        hexValue: "#FFFF00"
-    },
-    {
-        id: 4,
-        colorName: "Lime",
-        hexValue: "#00FF00"
-    },
-    {
-        id: 5,
-        colorName: "Pink",
-        hexValue: "#FFC0CB"
-    },
-    {
-        id: 6,
-        colorName: "Orange",
-        hexValue: "#FFA500"
-    },
-    {
-        id: 7,
-        colorName: "Olive",
-        hexValue: "#808000"
-    },
-    {
-        id: 8,
-        colorName: "Grey",
-        hexValue: "#808080"
-    },
-    {
-        id: 9,
-        colorName: "Indigo",
-        hexValue: "#4B0082"
-    },
-    
-];
+const btns = document.querySelectorAll("button");
+// console.log(btns);
 
-let genNum = x => Math.floor((Math.random() * x));
+const counterElement = document.getElementById("counter-val");
+let counter = 0;
 
-const changeBackground = () => {
+btns.forEach(function(btn){
+    if (btn.id == "counter-increment"){
+        btn.addEventListener("click", (e) => {
+            
+            console.log("POINTER EVENT DETECTED : " + e.target.id);
+            console.log("Add button clicked");
 
-    let colorId= -1;
-    colorId = genNum(10);
-    console.log(`SEARCHING FOR COLOR ID: ${colorId}`);
+            counter+=1;
+            counterElement.textContent = counter;
+            changeTxtColor();
+        });
+    } else if (btn.id == "counter-decrement"){
+        btn.addEventListener("click", (e) => {
+            console.log("POINTER EVENT DETECTED : " + e.target.id);
+            console.log("Subtract button clicked");
 
-    let colorName = "";
-    let colorHex = ""; 
+            counter-=1;
+            counterElement.textContent = counter;
+            changeTxtColor();
+        });
+    } else if (btn.id == "counter-reset"){
+        btn.addEventListener("click", (e) => {
+            console.log("POINTER EVENT DETECTED : " + e.target.id);
+            console.log("Reset button clicked");
 
-    colorObjs.forEach(function(color){
-        if (color.id === colorId){
-            console.log("COLOR LOCATED");
-            colorName = color.colorName;
-            colorHex = color.hexValue;
-        }
-    });
+            counter=0;
+            counterElement.textContent = counter;
+            changeTxtColor();
+        });
+    }
 
-    console.log("SWITCHING BACKGROUND TO: ");
-    console.log(`COLOR: ${colorName}`);
-    console.log(`HEX VALUE: ${colorHex}`);
+});
 
-    document.body.style.background = colorHex;
-    let crtClr = document.querySelector('#flipper-current');
-    crtClr.textContent = colorName;
-
-};
-
-let btn_element = document.querySelector("#flipper-btn");
-btn_element.addEventListener("click", changeBackground);
+function changeTxtColor(){
+    if (counter > 0){
+        counterElement.style.color = "green";
+    } else if (counter < 0){
+        counterElement.style.color = "orange";
+    } else {
+        counterElement.style.color = "indigo";
+    }
+}
