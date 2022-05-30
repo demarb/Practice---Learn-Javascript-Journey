98c33f947958941a4d42b5a545acd7dd2e3c6dd1
demarb <demarcbrown01@gmail.com>
2022-05-29 20:36:17 -0400

    Color changing on function call

--- 2_Beginner Vanilla JS Projects/1_Color_Flipper/main.js
+++ 2_Beginner Vanilla JS Projects/1_Color_Flipper/main.js
@@ -1,58 +1,78 @@
 const colorObjs = [
     {
-        id: 1,
+        id: 0,
         colorName: "Red",
         hexValue: "#FF0000"
     },
     {
-        id: 2,
+        id: 1,
         colorName: "Blue",
         hexValue: "#0000FF"
     },
     {
-        id: 3,
+        id: 2,
         colorName: "Purple",
         hexValue: "#800080"
     },
     {
-        id: 4,
+        id: 3,
         colorName: "Yellow",
         hexValue: "#FFFF00"
     },
     {
-        id: 5,
+        id: 4,
         colorName: "Lime",
         hexValue: "#00FF00"
     },
     {
-        id: 6,
+        id: 5,
         colorName: "Pink",
         hexValue: "#FFC0CB"
     },
     {
-        id: 7,
+        id: 6,
         colorName: "Orange",
         hexValue: "#FFA500"
     },
     {
-        id: 8,
+        id: 7,
         colorName: "Olive",
         hexValue: "#808000"
     },
     {
-        id: 9,
+        id: 8,
         colorName: "Grey",
         hexValue: "#808080"
     },
     {
-        id: 10,
+        id: 9,
         colorName: "Indigo",
         hexValue: "#4B0082"
     },
     
-]
+];
 
-// const changeBackground => {
+let genNum = (x=10) => Math.floor((Math.random() * x));
+// console.log(randNum());
 
-//     ;
-// }
+const changeBackground = (colorId= genNum()) => {
+
+    console.log(colorId);
+
+    colorObjs.forEach(function(color){
+        if (color.id === colorId){
+            colorName = color.colorName;
+            colorHex = color.hexValue;
+        }
+    });
+
+    console.log(colorName);
+    console.log(colorHex);
+
+    document.body.style.background = colorHex;
+    crtClr = document.querySelector('#flipper-current');
+    crtClr.textContent = colorName;
+
+};
+
+changeBackground();
