0fb812b436a1259936463816462110a7904f48ff
demarb <demarcbrown01@gmail.com>
2022-06-15 17:11:14 -0400

    Start modal

---
+++ 2_Beginner Vanilla JS Projects/6_Modal/main.js
@@ -0,0 +1,17 @@
+// toggleBtn = document.querySelector(".sidebar-toggle");
+// closeBtn = document.querySelector(".close-btn");
+// sidebar = document.querySelector(".sidebar");
+
+// console.log(toggleBtn);
+// console.log(closeBtn);
+// console.log(sidebar.classList);
+
+// toggleBtn.addEventListener("click", (e)=>{
+//     console.log("Inside action open listener");
+//     sidebar.classList.toggle("show-sidebar");
+// });
+
+// closeBtn.addEventListener("click", (e)=> {
+//     console.log("Inside action close listener");
+//     sidebar.classList.toggle("show-sidebar");
+// });
