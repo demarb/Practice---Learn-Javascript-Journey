4600c8bd9ef31301e620ec3e1adfa4be9bf88b86
demarb <demarcbrown01@gmail.com>
2022-06-15 12:32:27 -0400

    Finish sidebar

--- 2_Beginner Vanilla JS Projects/5_Sidebar Toggle/main.js
+++ 2_Beginner Vanilla JS Projects/5_Sidebar Toggle/main.js
@@ -1,24 +1,17 @@
-toggleBtn = document.querySelector(".toggle");
-links = document.querySelector(".links");
-console.log("Hello");
+toggleBtn = document.querySelector(".sidebar-toggle");
+closeBtn = document.querySelector(".close-btn");
+sidebar = document.querySelector(".sidebar");
+
 console.log(toggleBtn);
-console.log(links);
+console.log(closeBtn);
+console.log(sidebar.classList);
 
-toggleBtn.addEventListener("click", (e) => {
-    console.log("TOGGLE BEFORE");
-    console.log(links.classList);
-    
-    // if (links.classList.contains("hide-links")==true){
-    //     links.classList.remove("hide-links");
-    //     links.classList.add("show-links");
-    // }else{
-    //     links.classList.remove("show-links");
-    //     links.classList.add("hide-links");
-    // }
+toggleBtn.addEventListener("click", (e)=>{
+    console.log("Inside action open listener");
+    sidebar.classList.toggle("show-sidebar");
+});
 
-    //removing and adding can be done using toggle
-    links.classList.toggle("show-links");
-
-    console.log("TOGGLE AFTER");
-    console.log(links.classList);
+closeBtn.addEventListener("click", (e)=> {
+    console.log("Inside action close listener");
+    sidebar.classList.toggle("show-sidebar");
 });
