e884a9e6ba44e5c056d0794793672e00d71ba9dd
demarb <demarcbrown01@gmail.com>
2022-06-16 19:12:06 -0400

    Finish questions project

--- 2_Beginner Vanilla JS Projects/7_Questions/main.js
+++ 2_Beginner Vanilla JS Projects/7_Questions/main.js
@@ -1,10 +1,22 @@
 faqExpandBtns = document.querySelectorAll(".expand-faq");
 console.log(faqExpandBtns)
+faqResponse = document.querySelectorAll(".faq-response");
 
 faqExpandBtns.forEach(btn => {
     console.log(faqExpandBtns);
 
     btn.addEventListener("click", (e)=>{
-        
+        // Dom Traversal
+        let faqSelection = e.currentTarget.parentElement.parentElement;
+        expandFaq(faqSelection);
     });
-});
+})
+
+expandFaq = (faqSelection) => {
+    faqResponse.forEach(response => {
+        response.parentElement.firstElementChild.lastElementChild.firstElementChild.classList.remove("btn-rotate");
+        response.classList.remove("show-text");
+    });
+    faqSelection.firstElementChild.lastElementChild.firstElementChild.classList.add("btn-rotate");
+    faqSelection.lastElementChild.classList.add("show-text");
+}
